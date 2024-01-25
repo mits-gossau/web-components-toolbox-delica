a6de507f8bd5df3e813005d2457d1518de78cd11
Williams Pesic <williams.pesic@mgb.ch>
2024-01-25 15:52:34 +0100

    Test: Demo purposes GitHub Pages

--- wc-config.js
+++ wc-config.js
@@ -169,21 +169,7 @@
   // ↓↓↓ adjustable ↓↓↓
   './src/es/components/', // baseUrl
   [
-    {
-      selector: 'd-o-',
-      url: 'organismus/',
-      separateFolder: true
-    },
-    {
-      selector: 'd-m-',
-      url: 'molecules/',
-      separateFolder: true
-    },
-    {
-      selector: 'd-a-',
-      url: 'atoms/',
-      separateFolder: true
-    },
+    
     {
       selector: 'a-',
       url: 'web-components-toolbox/src/es/components/atoms/',
@@ -222,6 +208,21 @@
     {
       selector: 'third-party-',
       url: 'web-components-toolbox/src/es/components/thirdParty/',
+      separateFolder: true
+    },
+    {
+      selector: 'd-o-',
+      url: 'organismus/',
+      separateFolder: true
+    },
+    {
+      selector: 'd-m-',
+      url: 'molecules/',
+      separateFolder: true
+    },
+    {
+      selector: 'd-a-',
+      url: 'atoms/',
       separateFolder: true
     }
   ] // directories
