579aef0ade3ccfe149cee2a43e5b958c996aa6b6
Nukecola2005 <williams.pesic@gmail.com>
2024-10-14 08:45:34 +0200

    UPDATE: Changed Frontend Logic for Brandwrapper

--- src/es/components/molecules/brandWrapper/BrandWrapper.js
+++ src/es/components/molecules/brandWrapper/BrandWrapper.js
@@ -1,43 +1,39 @@
 import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
 
 export default class BrandWrapper extends Shadow() {
-  constructor(options = {}, ...args) {
+  constructor (options = {}, ...args) {
     super({ hoverInit: undefined, importMetaUrl: import.meta.url, ...options }, ...args)
   }
 
-  connectedCallback() {
+  connectedCallback () {
     if (this.shouldRenderCSS()) this.renderCSS()
   }
 
-  shouldRenderCSS() {
+  shouldRenderCSS () {
     return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
   }
 
-  renderCSS() {
+  renderCSS () {
     this.css = /* css */`
     :host{
       display: var(--any-display, block);
       flex-wrap: wrap;
       flex-direction: row;
-      justify-content: center;
-      gap: 0.5em;
+      justify-content: space-between;
+      gap: 1em;
       --any-display: var(--brand-wrapper-any-display, flex);
       --img-height: var(--brand-wrapper-img-height, 1%);
       --picture-scale-up-img-height: var(--brand-wrapper-picture-scale-up-img-height, 8em);
-      --picture-scale-up-img-width: var(--brand-wrapper-picture-scale-up-img-width, fit-content);
-      --wrapper-teaser-gap: 2.3em;
-      --picture-scale-up-img-width-mobile: 16em;
+      --picture-scale-up-img-width: var(--brand-wrapper-picture-scale-up-img-width, 10em);
 
     }
-
-    :host > section{
-      --wrapper-teaser-justify-content: space-between;
+    :host a{
+      flex: 1 0 21%;
     }
 
     @media only screen and (max-width: 767px) {
-      :host{
-        flex-direction: row;
-        flex-wrap: nowrap;
+      :host a{
+        flex: 1 0 46%;
       }
     }
 
