b08db3c57939ce05b9e9d5678a005a8f75100639
wpesicDev <williams.pesic@gmail.com>
2025-08-11 09:51:56 +0200

    fixed standorte picker & refactored code

--- src/es/components/molecules/standortePickerEinzel/StandortePickerEinzel.js
+++ src/es/components/molecules/standortePickerEinzel/StandortePickerEinzel.js
@@ -1,49 +1,52 @@
 import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
 
 export default class StandortePickerEinzel extends Shadow() {
-  constructor (options = {}, ...args) {
+  constructor(options = {}, ...args) {
     super({ hoverInit: undefined, importMetaUrl: import.meta.url, ...options }, ...args)
     this.hotspots = this.root.querySelectorAll('a-hotspot')
     this.descriptions = this.root.querySelectorAll('.description')
     this.currentDescription = this.root.querySelector(".description[id='1']")
-  }
-
-  connectedCallback () {
-    if (this.shouldRenderCSS()) this.renderCSS()
-    this.setStartingDescription()
-    document.body.addEventListener('click', this.setCurrentDescription)
-  }
-
-  disconnectedCallback () {
-    document.body.removeEventListener('click', this.setCurrentDescription)
-  }
-
-  setCurrentDescription = (event) => {
-    if (event) {
-      const composedPath = Array.from(event.composedPath())
-      if (composedPath.some(node => node.classList && node.classList.contains('btn-open'))) {
-        this.currentDescription.classList.remove('active')
-        if (composedPath[2].getAttribute('id') != this.currentDescription.getAttribute('id')) {
-          this.descriptions.forEach(desc => {
-            if (composedPath[2].getAttribute('id') === desc.getAttribute('id')) {
-              this.currentDescription = desc
-              this.currentDescription.classList.add('active')
-            }
-          })
+    this.setCurrentDescription = (event) => {
+      if (event) {
+        const composedPath = Array.from(event.composedPath())
+        if (composedPath.some(node => node.classList && node.classList.contains('btn-open'))) {
+          this.currentDescription.classList.remove('active')
+          if (composedPath[2].getAttribute('id') != this.currentDescription.getAttribute('id')) {
+            this.descriptions.forEach(desc => {
+              if (composedPath[2].getAttribute('id') === desc.getAttribute('id')) {
+                this.currentDescription = desc
+                this.currentDescription.classList.add('active')
+              }
+            })
+          } else {
+            this.setStartingDescription()
+          }
         } else {
           this.setStartingDescription()
         }
-      } else {
-        this.setStartingDescription()
       }
     }
   }
 
-  setStartingDescription () {
+  connectedCallback() {
+    if (this.shouldRenderCSS()) this.renderCSS()
+    this.setStartingDescription()
+    this.hotspots.forEach(hotspot => {
+      hotspot.addEventListener('click', event => this.setCurrentDescription(event))
+    })
+  }
+
+  disconnectedCallback() {
+    this.hotspots.forEach(hotspot => {
+      hotspot.removeEventListener('click', event => this.setCurrentDescription(event))
+    })
+  }
+
+  setStartingDescription() {
     this.descriptions.forEach(desc => {
       if (desc.getAttribute('id') === 'default') {
         if (this.currentDescription) {
-        this.currentDescription.classList.remove('active')
+          this.currentDescription.classList.remove('active')
         }
         this.currentDescription = desc
         this.currentDescription.classList.add('active')
@@ -51,11 +54,11 @@ export default class StandortePickerEinzel extends Shadow() {
     })
   }
 
-  shouldRenderCSS () {
+  shouldRenderCSS() {
     return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
   }
 
-  renderCSS () {
+  renderCSS() {
     this.css = /* css */`
       :host {
         width: var(--StandortePickerEinzel-width, 100%);
